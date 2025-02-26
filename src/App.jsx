64360919eacbe8266ea1304230bb8b44e3502f2c
AxieCowboy <hmhroko@gmail.com>
2025-02-26 12:37:26 -0700

    button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState(null)
+
+  const fetchRandomPokemon = async () => {
+    const randomID = Math.floor(Math.random() * 151) + 1
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+      if (!response.ok) throw new Error('PKMN not found!')
+      const data = await response.json()
+      setPokemon(data)
+    } catch (error) {
+      setPokemon(null)
+    }
+  }
+
+  useEffect(() => {
+    fetchRandomPokemon()
+  }, [])
 
   return (
-    <>
-      Hello World
-    </>
+    <div>
+      <h1>PKMN PICK</h1>
+      <button onClick={fetchRandomPokemon}>New PKMN</button>
+      {pokemon ? (
+        <div>
+          <h2>{pokemon.name.toUpperCase()}</h2>
+          { <img src={pokemon.sprites.front_default} /> /* PKMN IMG */}
+          <p>Type: {pokemon.types.map((t) => t.type.name).join(', ')}</p>
+        </div>
+      ) : (
+        <p>PKMN not found!</p>
+      )}
+    </div>
   )
 }
 
